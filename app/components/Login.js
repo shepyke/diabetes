f70edeb4e53820075a4d92dae2a4a6926ad65600
Mate Pasztor <mate.pasztor@gravityrd.com>
2018-01-11 12:00:19 +0100

    update Login feature

--- app/components/Login.js
+++ app/components/Login.js
@@ -14,7 +14,6 @@ import {
     TouchableOpacity,
     AsyncStorage,
 } from 'react-native';
-import Navigator from "../config/Navigator";
 import Styles from "../config/Styles";
 
 export default class Login extends Component<{}> {
@@ -22,12 +21,22 @@ export default class Login extends Component<{}> {
     constructor(props){
         super(props);
         this.state = {
+            user: {
                 username: '',
                 password: '',
+                firstName: '',
+                lastName: '',
+                email: '',
+                profileImage: '',
+                birthDate: '',
+                gender: '',
+                type: '',
+                bloodSugarAVG: '',
+            }
         }
     }
 
-    componentWillMount(){
+    componentDidMount(){
         this._loadInitialState().done();
     }
 
@@ -39,22 +48,22 @@ export default class Login extends Component<{}> {
     }
 
     login = () => {
-        fetch('http://192.168.0.102:3000/users',{
+        fetch('http://192.168.25.223:3000/users/login',{
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                username: this.state.username,
-                passwords: this.state.password,
+                username: this.state.user.username,
+                password: this.state.user.password,
             })
         })
 
         .then((response) => response.json())
         .then ((res) => {
             if(res.success === true){
-                AsyncStorage.setItem('user', res.user);
+                AsyncStorage.setItem('user', JSON.stringify(res.user));
                 this.props.navigation.navigate('Profile');
             }else{
                 alert(res.message);
@@ -69,52 +78,67 @@ export default class Login extends Component<{}> {
 
     render() {
         return (
-            <KeyboardAvoidingView id='login' behavior='padding' style={Styles.wrapper}>
-                <View style={Styles.container}>
+            <View style={Styles.wrapper}>
 
-                    <Text style={Styles.header}>LOGIN</Text>
+                <KeyboardAvoidingView id='login' behavior='padding'>
+                </KeyboardAvoidingView>
 
-                    <TextInput
-                        style={Styles.textInput}
-                        placeholder='Username'
-                        onChangeText={ (username) => this.setState( {username }) }
-                        underlineColorAndroid='transparent'
-                    />
+                    <View style={Styles.container}>
 
-                    <TextInput
-                        style={Styles.textInput}
-                        placeholder='Password'
-                        onChangeText={ (password) => this.setState( {password }) }
-                        underlineColorAndroid='transparent'
-                        secureTextEntry={true}
-                    />
+                        <Text style={Styles.header}>LOGIN</Text>
 
-                    <TouchableOpacity
-                        style={Styles.button}
-                        onPress={this.login}>
-                        <Text>Log in </Text>
-                    </TouchableOpacity>
+                        <TextInput
+                            style={Styles.textInput}
+                            placeholder='Username'
+                            onChangeText={
+                                (username) => {
+                                    const user = Object.assign({},
+                                        this.state.user, { username: username });
+                                    this.setState({ user: user });
+                            }}
+                            underlineColorAndroid='transparent'
+                        />
 
-                    <Text style={Styles.bottomtext}>
-                        <Text>Forgot your password? </Text>
-                        <Text
-                            style={Styles.register}
-                            onPress={this.goToRegistration}>
-                            Reset my password!
+                        <TextInput
+                            style={Styles.textInput}
+                            placeholder='Password'
+                            onChangeText={
+                                (password) => {
+                                    const user = Object.assign({},
+                                        this.state.user, { password: password });
+                                    this.setState({ user: user });
+                                }}
+                            underlineColorAndroid='transparent'
+                            secureTextEntry={true}
+                        />
+
+                        <TouchableOpacity
+                            style={Styles.button}
+                            onPress={this.login}>
+                            <Text>Log in </Text>
+                        </TouchableOpacity>
+
+                        <Text style={Styles.bottomtext}>
+                            <Text>Forgot your password? </Text>
+                            <Text
+                                style={Styles.register}
+                                onPress={this.goToRegistration}>
+                                Reset my password!
+                            </Text>
                         </Text>
-                    </Text>
 
-                    <Text style={Styles.bottomtext}>
-                        <Text>Not registered yet? </Text>
-                        <Text
-                            style={Styles.register}
-                            onPress={this.goToRegistration}>
-                                Register now!
+                        <Text style={Styles.bottomtext}>
+                            <Text>Not registered yet? </Text>
+                            <Text
+                                style={Styles.register}
+                                onPress={this.goToRegistration}>
+                                    Register now!
+                            </Text>
                         </Text>
-                    </Text>
 
-                </View>
-            </KeyboardAvoidingView>
+                    </View>
+
+            </View>
         );
     }
 }
