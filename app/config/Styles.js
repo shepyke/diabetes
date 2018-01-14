3d4fde09df6fda0b5b97215c373896fcf93f8b76
Mate Pasztor <mate.pasztor@gravityrd.com>
2018-01-14 11:41:49 +0100

    add Styles to the pages

--- app/config/Styles.js
+++ app/config/Styles.js
@@ -2,8 +2,15 @@ import { StyleSheet } from 'react-native';
 import React, { Component } from 'react';
 
 const Styles = StyleSheet.create({
-    wrapper:{
+    wrapper: {
         flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#1c313a',
+    },
+    rowContainer: {
+        flex: 0.2,
+        flexDirection: 'row',
+        backgroundColor: '#1c313a',
     },
     container: {
         flex: 1,
@@ -21,14 +28,49 @@ const Styles = StyleSheet.create({
         borderBottomColor: '#718792',
         borderBottomWidth: 1,
     },
+    footer: {
+        flex: .3,
+        padding: 20,
+        alignSelf: 'stretch',
+    },
+    welcome: {
+        fontSize: 20,
+        color: '#fff',
+        textAlign: 'center',
+        paddingBottom: 10,
+        fontWeight: 'bold',
+        borderBottomColor: '#718792',
+        borderBottomWidth: 1,
+    },
+    backgroundImage: {
+        width: undefined,
+        height: undefined,
+        backgroundColor:'transparent',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20,
+        padding: 10,
+        borderBottomColor: 'black',
+        borderBottomWidth: 2,
+    },
+    profileImage: {
+        height: 150,
+        width: 150,
+        borderRadius: 150 / 2, //make this a circle
+    },
+    birthDate: {
+        width: '100%',
+        padding: 8,
+        marginBottom: 10,
+    },
     text: {
         color: '#fff',
         fontWeight: 'bold',
     },
     textInput: {
         alignSelf: 'stretch',
-        padding: 8,
-        marginBottom: 10,
+        padding: 6,
+        marginBottom: 6,
         color: '#fff',
     },
     button: {
@@ -46,6 +88,13 @@ const Styles = StyleSheet.create({
         color: '#c2c2c2',
         fontSize: 8,
         fontWeight: 'bold',
+    },
+    logout: {
+        alignSelf: 'stretch',
+        backgroundColor: '#DD1A00',
+        padding: 10,
+        marginBottom: 20,
+        alignItems: 'center',
     },
 });
 
