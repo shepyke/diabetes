f1563db00d89708ff749c41b40fe38e1ac1a4bda
Mate Pasztor <mate.pasztor@gravityrd.com>
2018-01-14 11:41:21 +0100

    Fill up valid code the RegForm

--- app/components/RegistrationForm.js
+++ app/components/RegistrationForm.js
@@ -14,25 +14,81 @@ import {
     TouchableOpacity,
     AsyncStorage,
 } from 'react-native';
-import Navigator from "../config/Navigator";
 import Styles from "../config/Styles";
+import DatePicker from 'react-native-datepicker';
+import RadioForm,
+    {
+        RadioButton,
+        RadioButtonInput,
+        RadioButtonLabel
+    } from 'react-native-simple-radio-button';
+
+var gender_props = [
+    {label: 'Female    ', value: 'F' },
+    {label: 'Male', value: 'M' }
+];
 
 export default class RegistrationForm extends Component<{}> {
 
     constructor(props){
         super(props);
         this.state = {
-            firstName: '',
-            lastName: '',
-            email: '',
-            username: '',
-            password: '',
-            repassword: '',
+            user: {
+                username: '',
+                password: '',
+                repassword: '',
+                firstName: '',
+                lastName: '',
+                email: '',
+                profileImage: '',
+                birthDate: '',
+                gender: '',
+                type: '',
+                bloodSugarAVG: '',
+            }
         }
     }
 
-    submit = () => {
-        alert('siker');
+    submit = async() => {
+       try {
+            fetch('http://172.20.10.12:3000/users/registration', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    user:{
+                        firstName: this.state.user.firstName,
+                        lastName: this.state.user.lastName,
+                        username: this.state.user.username,
+                        email: this.state.user.email,
+                        birthDate: this.state.user.birthDate,
+                        //profileImage: this.state.user.profileImage,
+                        gender: this.state.user.gender,
+                        type: this.state.user.type,
+                        //bloodSugarAVG: this.state.user.bloodSugarAVG,
+                        password: this.state.user.password,
+                        repassword: this.state.user.repassword,
+                    }
+                })
+            })
+            .then((response) => response.json())
+            .then ((res) => {
+                if(res.success === true){
+                    console.log("hopp itt a response: " + res.user);
+                    AsyncStorage.setItem('user', JSON.stringify(res.user));
+                    this.props.navigation.navigate('Profile');
+                    alert('You have successfully registered');
+                }else{
+                    alert(res.message);
+                }
+            })
+            .done();
+        }catch(err){
+           console.log(err);
+       }
+
     }
 
     goToLogin = () => {
@@ -41,31 +97,65 @@ export default class RegistrationForm extends Component<{}> {
 
     render() {
         return (
-            <KeyboardAvoidingView id='registration' behavior='padding' style={Styles.wrapper}>
+            <View style={Styles.wrapper}>
+                <KeyboardAvoidingView id='registration' behavior='padding' />
                 <View style={Styles.container}>
 
                     <Text style={Styles.header}>Registration</Text>
 
-                    <TextInput
-                        style={Styles.textInput}
-                        placeholder='First Name'
-                        onChangeText={ (firstName) => this.setState( {firstName }) }
-                        underlineColorAndroid='transparent'
-                        placeholderTextColor='#718792'
-                    />
+                    <View style={Styles.rowContainer}>
 
-                    <TextInput
-                        style={Styles.textInput}
-                        placeholder='Last name'
-                        onChangeText={ (lastName) => this.setState( {lastName }) }
-                        underlineColorAndroid='transparent'
-                        placeholderTextColor='#718792'
+                        <TextInput
+                            style={[Styles.textInput, {width: '45%'}]}
+                            placeholder='First Name'
+                            onChangeText={
+                                (firstName) => {
+                                    const user = Object.assign({},
+                                        this.state.user, { firstName: firstName });
+                                    this.setState({ user: user });
+                                }}
+                            underlineColorAndroid='transparent'
+                            placeholderTextColor='#718792'
+                        />
+
+                        <TextInput
+                            style={[Styles.textInput, {width: '45%', marginLeft: '7%'}]}
+                            placeholder='Last name'
+                            onChangeText={
+                                (lastName) => {
+                                    const user = Object.assign({},
+                                        this.state.user, { lastName: lastName });
+                                    this.setState({ user: user });
+                                }}
+                            underlineColorAndroid='transparent'
+                            placeholderTextColor='#718792'
+                        />
+                    </View>
+
+                    <RadioForm
+                        radio_props={gender_props}
+                        labelColor={'#718792'}
+                        initial={'F'}
+                        formHorizontal={true}
+                        labelHorizontal={true}
+                        buttonColor={'#718792'}
+                        animation={true}
+                        onPress={(gender) => {
+                            const user = Object.assign({},
+                                this.state.user, { gender: gender });
+                            this.setState({ user: user })
+                        }}
                     />
 
                     <TextInput
                         style={Styles.textInput}
                         placeholder='Email'
-                        onChangeText={ (email) => this.setState( { email }) }
+                        onChangeText={
+                            (email) => {
+                                const user = Object.assign({},
+                                    this.state.user, { email: email });
+                                this.setState({ user: user });
+                            }}
                         underlineColorAndroid='transparent'
                         placeholderTextColor='#718792'
                     />
@@ -73,28 +163,76 @@ export default class RegistrationForm extends Component<{}> {
                     <TextInput
                         style={Styles.textInput}
                         placeholder='Username'
-                        onChangeText={ (username) => this.setState( {username }) }
+                        onChangeText={
+                            (username) => {
+                                const user = Object.assign({},
+                                    this.state.user, { username: username });
+                                this.setState({ user: user });
+                            }}
                         underlineColorAndroid='transparent'
                         placeholderTextColor='#718792'
                     />
 
-                    <TextInput
-                        style={Styles.textInput}
-                        placeholder='Password'
-                        placeholderTextColor='#718792'
-                        onChangeText={ (password) => this.setState( {password }) }
-                        underlineColorAndroid='transparent'
-                        secureTextEntry={true}
+                    <DatePicker
+                        style={Styles.birthDate}
+                        date={this.state.user.birthDate}
+                        mode="date"
+                        placeholder="Select your birthday"
+                        format="YYYY-MM-DD"
+                        minDate="1900-01-01"
+                        maxDate={new Date()}
+                        confirmBtnText="Confirm"
+                        cancelBtnText="Cancel"
+                        showIcon={false}
+                        customStyles={{
+                            dateInput: {
+                                borderWidth: 0,
+                                position: 'absolute',
+                                marginLeft: 0,
+                                left: 0,
+                            },
+                            dateText: {
+                                color: '#fff',
+                            },
+                            placeholderText: {
+                                color: '#718792',
+                            },
+                        }}
+                        onDateChange={(birthDate) => {
+                            const user = Object.assign({},
+                                this.state.user, { birthDate: birthDate });
+                            this.setState({ user: user });
+                        }}
                     />
+                    <View style={Styles.rowContainer}>
+                        <TextInput
+                            style={[Styles.textInput, {width: '45%'}]}
+                            placeholder='Password'
+                            placeholderTextColor='#718792'
+                            onChangeText={
+                                (password) => {
+                                    const user = Object.assign({},
+                                        this.state.user, { password: password });
+                                    this.setState({ user: user });
+                                }}
+                            underlineColorAndroid='transparent'
+                            secureTextEntry={true}
+                        />
 
-                    <TextInput
-                        style={Styles.textInput}
-                        placeholder='Password again'
-                        placeholderTextColor='#718792'
-                        onChangeText={ (repassword) => this.setState( {repassword }) }
-                        underlineColorAndroid='transparent'
-                        secureTextEntry={true}
-                    />
+                        <TextInput
+                            style={[Styles.textInput, {width: '45%', marginLeft: '7%'}]}
+                            placeholder='Password again'
+                            placeholderTextColor='#718792'
+                            onChangeText={
+                                (repassword) => {
+                                    const user = Object.assign({},
+                                        this.state.user, { repassword: repassword });
+                                    this.setState({ user: user });
+                                }}
+                            underlineColorAndroid='transparent'
+                            secureTextEntry={true}
+                        />
+                    </View>
 
                     <TouchableOpacity
                         style={Styles.button}
@@ -112,7 +250,7 @@ export default class RegistrationForm extends Component<{}> {
                     </Text>
 
                 </View>
-            </KeyboardAvoidingView>
+            </View>
         );
     }
 }
