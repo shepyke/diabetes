79f66aa08eb6dd5a2c3af01bdcc6ea6556552bdc
Mate Pasztor <mate.pasztor@gravityrd.com>
2018-04-27 03:25:39 +0200

    step 0.1 to photo upload

--- android/app/src/main/java/com/diabetes/MainApplication.java
+++ android/app/src/main/java/com/diabetes/MainApplication.java
@@ -3,6 +3,9 @@ package com.diabetes;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
+import fr.bamlab.rnimageresizer.ImageResizerPackage;
+import com.imagepicker.ImagePickerPackage;
 import org.reactnative.camera.RNCameraPackage;
 import io.realm.react.RealmReactPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +28,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFSPackage(),
+            new ImageResizerPackage(),
+            new ImagePickerPackage(),
             new RNCameraPackage(),
             new RealmReactPackage()
       );
