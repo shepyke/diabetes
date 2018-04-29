bd1f1f9c9d23c95e85acc35952283887fa8c993d
Mate Pasztor <mate.pasztor@gravityrd.com>
2018-04-29 15:45:45 +0200

    add hash module to configs

--- android/app/src/main/java/com/diabetes/MainApplication.java
+++ android/app/src/main/java/com/diabetes/MainApplication.java
@@ -3,6 +3,7 @@ package com.diabetes;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.sha256lib.Sha256Package;
 import com.rnfs.RNFSPackage;
 import fr.bamlab.rnimageresizer.ImageResizerPackage;
 import com.imagepicker.ImagePickerPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new Sha256Package(),
             new RNFSPackage(),
             new ImageResizerPackage(),
             new ImagePickerPackage(),
